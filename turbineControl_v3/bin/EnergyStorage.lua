e7e78d496dbc1923bae333f991138f8b45a53723
Thor_s_Crafter <thorscrafter@gmail.com>
2017-10-09 16:57:10 +0200

    Added EnergyStorage Class

--- turbineControl_v3/bin/EnergyStorage.lua
+++ turbineControl_v3/bin/EnergyStorage.lua
@@ -2,10 +2,41 @@
 -- (c) 2017 Thor_s_Crafter
 -- Version 3.0
 
--- Energy Storage class
+local EnergyStorage = {
+    name = "",
+    id = {},
+    
+    energy = function(self)
+        return self.id.getEnergyStored()
+    end,
+    capacity = function(self)
+        return self.id.getCapacityStored()
+    end,
+    percentage = function(self)
+        return math.floor(self.energy/self.capacity*100)
+    end,
+    percentagePrecise = function(self)
+        return self.energy/self.capacity*100
+    end
+}
 
--- TODO
+function newEnergyStorage(name,id)
+    local storage = {}
+    setmetatable(storage,{__index=EnergyStorage})
 
+    storage.name = name
+    storage.id = id
+
+    return storage
+end
+
+function printEnergyStorageData(storage)
+    print("Name: "..storage.name)
+    print("ID: "..tostring(storage.id))
+    print("Energy: "..storage:energy())
+    print("Capacity: "..storage:capacity())
+    print("Fill: "..storage:percentage().."%")
+end
 
 
 
